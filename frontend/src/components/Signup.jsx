a294be00f393b191da8134a39add6cf37903e545
Mayoorthanan <mayoorthanan2004@gmail.com>
2025-12-29 14:57:10 +0530

    Update backend API URL

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -8,7 +8,9 @@ export default function Signup() {
   const nav = useNavigate();
 
   const signup = async () => {
-    await axios.post("http://localhost:5000/api/auth/signup", form);
+    axios.post(
+  "https://8a69333f-b382-4724-ae7e-d2b36ab57abb-00-2pofjhqx85ni.sisko.replit.dev/api/auth/login"
+);
     nav("/");
   };
 
